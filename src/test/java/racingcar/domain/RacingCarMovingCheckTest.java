09d8d34bc2cfd3a37683330014f62bf8d74bc77c
sanghyun-k <sanghyun.k@navercorp.com>
2022-04-25 22:29:50 +0900

    자동차가 전진하기 위한 조건 Test Code

--- src/test/java/racingcar/domain/RacingCarMovingCheckTest.java
+++ src/test/java/racingcar/domain/RacingCarMovingCheckTest.java
@@ -1,4 +1,38 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import racingcar.utils.RandomNumber;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
 class RacingCarMovingCheckTest {
-  
+
+    private RandomNumber randomNumber;
+    private RacingCarMovingCheck racingCarMovingCheck;
+
+    @BeforeEach
+    void 테스트_준비() {
+        randomNumber = mock(RandomNumber.class);
+        racingCarMovingCheck = new RacingCarMovingCheck(randomNumber);
+    }
+
+
+    @Test
+    void 전진_조건_성공() {
+        doReturn(4).when(randomNumber).generator();
+        assertThat(racingCarMovingCheck.isMovable()).isTrue();
+    }
+
+    @Test
+    void 전진_조건_실패() {
+        doReturn(3).when(randomNumber).generator();
+        assertThat(racingCarMovingCheck.isMovable()).isFalse();
+    }
 }
--- src/test/java/racingcar/utils/RandomNumberTest.java
+++ src/test/java/racingcar/utils/RandomNumberTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.utils;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class RandomNumberTest {
-  
+    private  RandomNumber randomNumber;
+
+    @BeforeEach
+    void 테스트_준비() {
+        randomNumber = new RandomNumber();
+    }
+
+    @Test
+    void 랜덤숫자_범위_체크() {
+        assertThat(randomNumber.generator()).isBetween(0, 9);
+    }
+
 }
